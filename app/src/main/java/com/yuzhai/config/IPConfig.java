7a536d0d1de1f853480b8ceaf195607715c0ef30
HJF <354297098@qq.com>
2016-05-14 21:43:57 +0800

    完成了存储IP信息的接口

---
+++ app/src/main/java/com/yuzhai/config/IPConfig.java
@@ -0,0 +1,11 @@
+package com.yuzhai.config;
+
+/**
+ * 创建时间：2016/5/14
+ * 作者：HJF
+ * 主要功能：IP地址相关的信息
+ */
+public interface IPConfig {
+    final static String IP_ADDRESS = "172.16.178.83";
+    final static String IP_PORT = "8080";
+}
