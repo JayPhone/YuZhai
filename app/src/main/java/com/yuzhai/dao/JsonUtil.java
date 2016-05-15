a28f5a6e955259911bdf7dc0430b368edcb8fde7
HJF <354297098@qq.com>
2016-05-15 14:34:45 +0800

    添加JsonUtil类

--- app/src/main/java/com/yuzhai/dao/JsonUtil.java
+++ app/src/main/java/com/yuzhai/dao/JsonUtil.java
@@ -1,7 +1,47 @@
 package com.yuzhai.dao;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 /**
- * Created by Administrator on 2016/5/14.
+ * 创建时间2016/5/14.
+ * 作者 HJF
+ * 主要功能：JSON数据的解析
  */
 public class JsonUtil {
+
+    public static <T> T getPerson(String jsonString, Class<T> cls) {
+        T t = null;
+        Gson gson = new Gson();
+        t = gson.fromJson(jsonString, cls);
+        return t;
+    }
+
+    public static <T> List<T> getPersons(String jsonString) {
+        List<T> list = new ArrayList<T>();
+        Gson gson = new Gson();
+        list = gson.fromJson(jsonString, new TypeToken<List<T>>() {
+        }.getType());
+        return list;
+    }
+
+    public static <T> List<T> getCitys(String jsonString) {
+        List<T> list = new ArrayList<T>();
+        Gson gson = new Gson();
+        list = gson.fromJson(jsonString, new TypeToken<List<T>>() {
+        }.getType());
+        return list;
+    }
+
+    public static <T> List<Map<String, T>> getListMap(String jsonString) {
+        List<Map<String, T>> list = new ArrayList<Map<String, T>>();
+        Gson gson = new Gson();
+        list = gson.fromJson(jsonString, new TypeToken<List<Map<String, T>>>() {
+        }.getType());
+        return list;
+    }
 }
