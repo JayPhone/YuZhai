c8826bc6cb48a01b4f49e4926ca3e27df3925efb
lvdev <995059110@qq.com>
2016-07-17 14:36:03 +0800

    2016.7.17 1.软件设置界面 2.头像界面

---
+++ app/src/main/java/com/yuzhai/ui/FeedBackActivity.java
@@ -0,0 +1,15 @@
+package com.yuzhai.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yuzhai.yuzhaiwork.R;
+
+public class FeedBackActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed_back);
+    }
+}
